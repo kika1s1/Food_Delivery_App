04351cce09eefed3957d5f9cd624fce98f0b053c
Tamirat Kebede <tamiratkebede120@gmail.com>
2024-05-28 18:03:04 +0300

    gradle is fixed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: const Column(
+        children: [
+          // logo
+          // app slogan
+          // email
+          // password
+          // signin
+          // not a member
+
+        ],),
+    );
+  }
+}
---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:food_delivery/themes/dark_mode.dart';
+import 'package:food_delivery/themes/light_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData =>_themeData;
+
+  bool get isDarkMode =>_themeData == darkMode;
+
+  set themeData(ThemeData themeData){
+    _themeData = themeData;
+    notifyListeners();
+  }
+  void toggleTheme(){
+    themeData = _themeData == darkMode ? lightMode : darkMode;
+
+  }
+  
+}
